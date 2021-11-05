7e590b9154a89acd45a085b6d9548b01886463e6
Lance Tipton <lancetipton04@gmail.com>
2021-11-05 05:21:14 -0700

    feat(tasks): Add tap open task

---
+++ src/tasks/tap/open.js
@@ -0,0 +1,31 @@
+const { runInternalTask } = require('KegUtils/task/runInternalTask')
+
+
+/**
+ * Opens tap folder path into the editor set in the globalConfig
+ * @function
+ * @param {Object} args - arguments passed from the runTask method
+ * @param {Object} args.globalConfig - Global config object for the keg-cli
+ *
+ * @returns {void}
+ */
+const tapOpen = args => {
+  const { params } = args
+
+  return runInternalTask('tasks.open', {
+    ...args,
+    command: 'open',
+    params: { name: params?.tap },
+    options: [ params.tap, ...args.options ],
+  })
+
+}
+
+module.exports = {
+  open: {
+    name: 'open',
+    action: tapOpen,
+    description: `Opens a tap in the settings defined editor`,
+    example: 'keg tap open'
+  }
+}
