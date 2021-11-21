5e5a85ffba2233d9e4b644fe323af861d58fcea5
Lance Tipton <lancetipton04@gmail.com>
2021-11-21 06:35:44 -0700

    fix(parse-config): Fix failing tests in ci

--- repos/parse-config/src/loadConfigs/__tests__/loadConfigs.js
+++ repos/parse-config/src/loadConfigs/__tests__/loadConfigs.js
@@ -3,11 +3,26 @@ jest.clearAllMocks()
 
 const path = require('path')
 const homeDir = require('os').homedir()
-const { getKegGlobalConfig } = require('@keg-hub/cli-utils')
+
+const yml = require('../../yml/yml')
+const env = require('../../env/env')
+
+const loadYmlSyncMock = jest.fn((...args) => yml.loadYmlSync(...args))
+jest.setMock('../../yml/yml', {
+  ...yml,
+  loadYmlSync: loadYmlSyncMock
+})
+const loadEnvSyncMock = jest.fn((...args) => env.loadEnvSync(...args))
+jest.setMock('../../env/env', {
+  ...env,
+  loadEnvSync: loadEnvSyncMock
+})
 
 const {
-  removeYmlFile,
   writeYmlFile,
+  removeYmlFile,
+  ymlSearchPaths,
+  envSearchPaths,
 } = require('../../__mocks__')
 
 const mocksDir = path.join(__dirname, '../../__mocks__')
@@ -32,26 +47,11 @@ describe('Loaders.loadConfigs', () => {
 
   afterEach(async () => {
     await removeYmlFile(testYmlData)
+    loadYmlSyncMock.mockClear()
+    loadEnvSyncMock.mockClear()
   })
 
   afterAll(() => jest.resetAllMocks())
-
-  it(`should load default values from the default paths`, () => {
-    const loaded = loadConfigs({ name: 'parse' })
-
-    Object.keys(testYmlData.env).map(key => {
-      expect(loaded[key]).not.toBe(undefined)
-      expect(typeof loaded[key]).toBe(typeof testYmlData.env[key])
-    })
-  })
-
-  it(`should the template values should be filled`, () => {
-    const loaded = loadConfigs({ name: 'parse' })
-
-    const globalConfig = getKegGlobalConfig()
-    expect(loaded.defaultEnv).toBe(globalConfig.cli.settings.defaultEnv)
-    expect(loaded.Object).toEqual({ [globalConfig.name]: globalConfig.version })
-  })
 
   it(`should load a config from a custom locations for yml files`, () => {
     const loaded = loadConfigs({ name: 'parse', locations: [ mocksDir ]})
@@ -63,4 +63,46 @@ describe('Loaders.loadConfigs', () => {
     expect(loaded.CUSTOM_ENV_TEST_LOCATION).toBe('/custom/env/test/location')
   })
 
+  it(`should call the load sync method for the correct config type`, () => {
+    loadConfigs({ noEnv: true, name: 'parse' })
+    expect(loadYmlSyncMock).toHaveBeenCalled()
+    expect(loadEnvSyncMock).not.toHaveBeenCalled()
+
+    loadYmlSyncMock.mockClear()
+    loadEnvSyncMock.mockClear()
+
+    loadConfigs({ noYml: true, name: 'parse' })
+    expect(loadEnvSyncMock).toHaveBeenCalled()
+    expect(loadYmlSyncMock).not.toHaveBeenCalled()
+
+  })
+
+  it(`should call the load sync method with the correct paths`, () => {
+    loadConfigs({ noEnv: true, name: 'parse' })
+
+    loadYmlSyncMock.mock.calls.map(mockCall => {
+      const args = mockCall[0]
+      const ymlLoc = args.location
+      const cleanedLoc = ymlLoc.includes(`keg-cli/repos/`)
+        ? ymlLoc.split(`keg-cli/repos/`).pop()
+        : ymlLoc.split('/').slice(3).join('/')
+
+      expect(ymlSearchPaths.includes(cleanedLoc)).toBe(true)
+    })
+  })
+
+  it(`should call the load sync method with the correct paths`, () => {
+    loadConfigs({ noYml: true, name: 'parse' })
+
+    loadEnvSyncMock.mock.calls.map(mockCall => {
+      const args = mockCall[0]
+      const envLoc = args.location
+      const cleanedLoc = envLoc.includes(`keg-cli/repos/`)
+        ? envLoc.split(`keg-cli/repos/`).pop()
+        : envLoc.split('/').slice(3).join('/')
+
+      expect(envSearchPaths.includes(cleanedLoc)).toBe(true)
+    })
+  })
+  
 })
