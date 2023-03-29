3ef657a0c6ee87ce29f4f6fd592baf001ac300da
Lance Tipton <lancetipton04@gmail.com>
2023-03-29 09:03:50 -0700

    feat(git): Minor fixes and updates

--- repos/git-lib/src/__tests__/branch.js
+++ repos/git-lib/src/__tests__/branch.js
@@ -12,15 +12,28 @@ describe('branch', () => {
 
     it('should return a list of all branches for the passed in location', async () => {
       const res = await git.branch.list(cliRoot)
-
       expect(isArr(res)).toBe(true)
     })
 
     it('should return an array of  branch objects matching the correct model', async () => {
       const res = await git.branch.list(cliRoot)
       const keys = [ 'commit', 'name', 'current', 'message' ]
-
       res.map(branch => keys.map(key => expect(key in branch).toBe(true)))
+    })
+
+  })
+
+  describe('get', () => {
+    it('should return a branch object for the passed in location', async () => {
+      const current = await git.branch.current({ location: cliRoot})
+
+      const branch = await git.branch.get(cliRoot, current.name)
+      expect(branch).toEqual(current)
+    })
+
+    it('should return null when the branch can not be found', async () => {
+      const res = await git.branch.get(cliRoot, 'branch-does-not-exists-4321')
+      expect(res).toBe(null)
     })
 
   })
--- repos/git-lib/src/git.js
+++ repos/git-lib/src/git.js
@@ -2,19 +2,34 @@ const gitUtils = require('./utils')
 const { Repo } = require('./repo')
 const { Remote } = require('./remote')
 const { Branch } = require('./branch')
-const { gitCmd } = require('./commands')
 const { Logger } = require('@keg-hub/cli-utils')
 const { gitSSHEnv, buildCmdOpts } = require('./utils/helpers')
+const { getKegGlobalConfig } = require('@keg-hub/cli-utils')
+
+const getGitConfig = () => {
+  const globalConfig = getKegGlobalConfig()
+  return globalConfig.cli.git
+}
 
 class Git {
 
-  constructor(options){
-    this.cmd = gitCmd
-    this.utils = gitUtils
-    this.branch = new Branch(this, options)
-    this.remote = new Remote(this, options)
-    this.repo = new Repo(this, options)
+  _isSetup = false
 
+  constructor(options){
+    options && this.setup(options)
+  }
+
+  setup = (options, force) => {
+    options = options || getGitConfig()
+
+    if(this._isSetup && !force) return
+
+    this._isSetup = true
+    this.branch = new Branch(this, options)
+    this.repo = new Repo(this, options)
+    this.remote = new Remote(this, options)
+    this.utils = gitUtils
+  
     options.sshKey && this.setSSHKey(options.sshKey)
   }
 
@@ -40,13 +55,27 @@ class Git {
   * @returns {void}
   */
   reset = async cmdOpts => {
-    await this.cmd(`clean -f .`, buildCmdOpts(cmdOpts, args))
-    return this.cmd(`reset --hard`, buildCmdOpts(cmdOpts, args))
+    await gitCmd(`git clean -f .`, buildCmdOpts(cmdOpts))
+    return gitCmd(`git reset --hard`, buildCmdOpts(cmdOpts))
+  }
+
+  /**
+  * Initializes a directory for a git repo
+  * @memberof Git
+  * @param {Object} cmdOpts - Options to pass to the spawnCmd
+  *
+  * @returns {boolean} - True if command is scuccessful, false otherwise
+  */
+  init = async cmdOpts => {
+    const [ err, resp ] = await limbo(gitCmd(`git init`, buildCmdOpts(cmdOpts)))
+    return err ? false : true
   }
 
 }
 
-const git = new Git({})
+
+const git = new Git()
+git.setup()
 
 module.exports = {
   Git,
